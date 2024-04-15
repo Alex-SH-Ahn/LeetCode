b48f69cbcc4af8224f65acf04fe819e92e460e6f
AlexAhn <144298841+Alex-SH-Ahn@users.noreply.github.com>
2024-04-15 23:31:58 +0900

    Create Leet682.py

---
+++ Leet682.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def calPoints(self, ops):
+        stack = []
+        for op in ops:
+            if op == 'C':
+                stack.pop()
+            elif op == 'D':
+                stack.append(stack[-1]*2)
+            elif op == '+':
+                stack.append(stack[-1]+stack[-2])
+            else:
+                stack.append(int(op))
+        return sum(stack)
+        
