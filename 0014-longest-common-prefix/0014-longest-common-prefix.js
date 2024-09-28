136f512ad711ef5f2c15c9fd2e763b84b893257f
AlexAhn <144298841+Alex-SH-Ahn@users.noreply.github.com>
2024-09-28 11:13:09 +0900

    Time: 53 ms (68.24%), Space: 49.3 MB (55.88%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.js
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -3,23 +3,24 @@
  * @return {string}
  */
 var longestCommonPrefix = function(strs) {
-    let shortest = strs[0];
+    let shrt = strs[0];
 
     for (let i = 1; i < strs.length; i++) {
-        if (strs[i].length < shortest.length) {
-            shortest = strs[i];
+        if (strs[i].length < shrt.length) {
+            shrt = strs[i];
         }
     }
 
-    let prefix='';
-    for (let i = 0; i < shortest.length; i++) {
+    let res='';
+    for (let i = 0; i < shrt.length; i++) {
         let char = strs[0][i];
         for (let j = 0; j < strs.length; j++) {
             if (strs[j][i] !== char) {
-                return prefix;
+                return res;
             }
         }
-        prefix += char;
+        res += char;
     }
-    return prefix
+    
+    return res
 };
