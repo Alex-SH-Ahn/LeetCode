a345b1c23a5bfed7ea437a1bbe0c231f90fbc9f4
AlexAhn <144298841+Alex-SH-Ahn@users.noreply.github.com>
2024-09-28 11:12:18 +0900

    Time: 58 ms (43.2%), Space: 49.2 MB (67.47%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.js
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -3,18 +3,23 @@
  * @return {string}
  */
 var longestCommonPrefix = function(strs) {
-    let shortest = strs.reduce((a, b) => a.length > b.length ? b : a);
+    let shortest = strs[0];
+
+    for (let i = 1; i < strs.length; i++) {
+        if (strs[i].length < shortest.length) {
+            shortest = strs[i];
+        }
+    }
+
     let prefix='';
-    for (let i = 0; i < shortest.length; i++) { // 가장 짧은 문자열의 길이만큼 순회
+    for (let i = 0; i < shortest.length; i++) {
         let char = strs[0][i];
-        let tf = true;
-        for (let j = 0; j < strs.length; j++) { // 배열 순회
+        for (let j = 0; j < strs.length; j++) {
             if (strs[j][i] !== char) {
-                tf = false;
                 return prefix;
             }
         }
-        prefix += (tf === false) ? '' : char;
+        prefix += char;
     }
     return prefix
 };
