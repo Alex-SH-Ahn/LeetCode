dcec7c5b3f5e3ba0ca63867ff61125ae4b5b46e1
AlexAhn <144298841+Alex-SH-Ahn@users.noreply.github.com>
2024-10-01 01:15:26 +0900

    Time: 61 ms (41.55%), Space: 50.8 MB (79.35%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.js
+++ 0605-can-place-flowers/0605-can-place-flowers.js
@@ -6,34 +6,10 @@
 var canPlaceFlowers = function(flowerbed, n) {
     let cnt=0;
     for (let i=0;i<flowerbed.length;i++){
-        if (flowerbed[i]===0) {
-            if (i===0 && flowerbed[i+1]===0) {
-                flowerbed[i]=1;
-                cnt++;
-                console.log(`first if: ${i}`)
-            } else if (flowerbed[i-1]===0 && flowerbed[i+1]===0) {
-                flowerbed[i]=1;
-                cnt++;
-                console.log(`second if: ${i}`)
-            } else if (i===flowerbed.length-1 && flowerbed[i-1]===0) {
-                flowerbed[i]=1;
-                cnt++;
-                console.log(`third if: ${i}`)
-            } else if (flowerbed.length===1) {
-                flowerbed[i]=1;
-                cnt++;
-                console.log(`fourth if: ${i}`)
-            }
+        if(flowerbed[i] === 0 && flowerbed[i-1] !== 1 && flowerbed[i+1] !== 1){
+            flowerbed[i] = 1
+            cnt++
         }
-        // if ((i===0) && (flowerbed[i+1] === 0) && (flowerbed[i]===0)) {
-        //     flowerbed[i]=1; cnt++;
-        //     console.log(`first if: ${i}`)
-        // } else if (flowerbed[i]===0 && flowerbed[i-1]===0 && flowerbed[i+1] === 0) {
-        //     flowerbed[i]=1; cnt++;
-        //     console.log(`second if: ${i}`)
-        // }
     }
-    console.log(cnt);
-    console.log(flowerbed);
-    return n <= cnt ? true : false;
+    return n <= cnt;
 };
