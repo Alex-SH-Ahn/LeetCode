3e8a59b60d436a8c88d7685a9003d45283e9a69c
AlexAhn <144298841+Alex-SH-Ahn@users.noreply.github.com>
2024-09-30 15:03:06 +0900

    Time: 51 ms (88.43%), Space: 50.6 MB (39.49%) - LeetHub

--- 1528-kids-with-the-greatest-number-of-candies/1528-kids-with-the-greatest-number-of-candies.js
+++ 1528-kids-with-the-greatest-number-of-candies/1528-kids-with-the-greatest-number-of-candies.js
@@ -5,13 +5,10 @@
  */
 var kidsWithCandies = function(candies, extraCandies) {
     let arr=candies.map((e)=>e+extraCandies);
-    console.log(`new arr:${arr}`);
     const max = Math.max(...candies);
-    console.log(`max: ${max}`);
     let res = arr.map((num, i)=>{
         if (num >= max) return true;
         else return false;
     });
-    console.log(res);
     return res;
 };
