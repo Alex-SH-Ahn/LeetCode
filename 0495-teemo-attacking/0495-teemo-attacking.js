7b556d6c4610d8f6231266d3c79c7a43415770bd
AlexAhn <144298841+Alex-SH-Ahn@users.noreply.github.com>
2024-10-02 10:16:33 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0495-teemo-attacking/0495-teemo-attacking.js
+++ 0495-teemo-attacking/0495-teemo-attacking.js
@@ -4,21 +4,17 @@
  * @return {number}
  */
 var findPoisonedDuration = function(timeSeries, duration) {
-    let sum = 0;
-    let prevEnd = 0;
-    
-    for (let i = 0; i < timeSeries.length; i++) {
-        let currentStart = timeSeries[i];
-        let currentEnd = currentStart + duration;
-        
-        if (currentStart < prevEnd) {
-            sum += currentEnd - prevEnd;
+    let sum=0;
+    let prev=0;
+    for (let i=0;i<timeSeries.length;i++) {
+        let currentStart=timeSeries[i];
+        let currentEnd=currentStart+duration;
+        if (currentStart<prev){
+            sum+=currentEnd-prev;
         } else {
-            sum += duration;
+            sum+=duration;
         }
-        
-        prevEnd = currentEnd;
+        prevEnd=currentEnd;
     }
-    
     return sum;
 };
