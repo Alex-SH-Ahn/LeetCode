939d4c9e4603cb05fec60f21a6a7c554e9eb0dc3
AlexAhn <144298841+Alex-SH-Ahn@users.noreply.github.com>
2024-10-06 22:48:17 +0900

    Time: 52 ms (67.42%), Space: 49.4 MB (39.83%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function(s, t) {
+    if (s.length > t.length) return false
+    let index=0;
+    for(let i=0;i<t.length;i++){
+        if(t[i]===s[index]) index++;
+    }
+    return index===s.length
+};
