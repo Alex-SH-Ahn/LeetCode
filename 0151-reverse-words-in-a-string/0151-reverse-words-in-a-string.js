7f39d50308f43cfb7085e1c5ff3ba86a52434f39
AlexAhn <144298841+Alex-SH-Ahn@users.noreply.github.com>
2024-10-09 08:06:25 +0900

    Time: 51 ms (78.49%), Space: 50.6 MB (71.79%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
@@ -3,12 +3,11 @@
  * @return {string}
  */
 var reverseWords = function(s) {
-    s=s.trim().split(/\s+/); // trim blank spaces & split them into words
-    let tmp=s.length-1;
-    for(let i=0;i<s.length/2;i++){
-        [s[i], s[tmp]] = [s[tmp], s[i]];
-        tmp--;
-        console.log(s);
-    }
-    return s.join(' ')
+    return s.trim().split(/\s+/).reverse().join(' '); // trim blank spaces & split them into words
+    // let tmp=s.length-1;
+    // for(let i=0;i<s.length/2;i++){
+    //     [s[i], s[tmp]] = [s[tmp], s[i]];
+    //     tmp--;
+    //     console.log(s);
+    // }
 };
