caaea1e6cf19ac52df8a5878dfab534f6d422416
AlexAhn <144298841+Alex-SH-Ahn@users.noreply.github.com>
2024-10-09 09:00:22 +0900

    Time: 79 ms (40.49%), Space: 54.3 MB (24.88%) - LeetHub

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.js
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.js
@@ -10,7 +10,6 @@ var maxVowels = function(s, k) {
     for(let i=0;i<sub.length;i++){
         if(vowels.includes(sub[i])) maxNum++;
     }
-    console.log('1: ',maxNum);
     let num=maxNum
 
     for(let i=0;i<s.length;i++){
